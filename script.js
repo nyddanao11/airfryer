822b54da645428efc721be4cf1ff1047b7e3bc0d
nyddanao11 <bonifacioamoren@gmail.com>
2023-01-18 18:26:39 +0800

    18jan2023

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-const primaryNav = document.getElementById('primary-navigation');
-const navToggle = document.getElementById('mobile-nav-toggle');
 
-navToggle.addEventListener('click', () => {
-  const visibility = primaryNav.getAttribute('data-visible');
-  if (visibility === 'false') {
-    primaryNav.setAttribute('data-visible', true);
-    navToggle.setAttribute('aria-expanded', true);
-  } else {
-    primaryNav.setAttribute('data-visible', false);
-    navToggle.setAttribute('aria-expanded', false);
+function showOuterDropdown1() {
+    document.getElementById("outerDropdown1").classList.toggle("show");
+}
+function showOuterDropdown() {
+    document.getElementById("outerDropdown").classList.toggle("show");
+}
+function showInnerDropdown1() {
+    document.getElementById("innerDropdown1").classList.toggle("show");
+}
+
+function showInnerDropdown2() {
+    document.getElementById("innerDropdown2").classList.toggle("show");
   }
-});
-document.addEventListener('touchend', function (event) {
-  if (!primaryNav.contains(event.target) && primaryNav.getAttribute('data-visible') === 'true') {
-    primaryNav.setAttribute('data-visible', false);
-    navToggle.setAttribute('aria-expanded', false);
-  }
-});
+ 
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("myDropdown");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};
+
